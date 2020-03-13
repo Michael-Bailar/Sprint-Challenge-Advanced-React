8df4736fecfbe822f77175fc6444397fdb1f4729
Michael-B1764 <mikebailar@gmail.com>
2020-03-13 10:01:35 -0700

    finished but playing around with uselocalstorage react test

--- client/src/components/Header.test.js
+++ client/src/components/Header.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
+import { render } from "@testing-library/react"
 
 it('Header renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Header />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('Header shows darkMode link', () => {
+  const { getByText } = render (<Header />)
+  const darkmode = getByText(/toggle darkmode/i)
+})
--- client/src/components/PlayersList.test.js
+++ client/src/components/PlayersList.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PlayersList from './PlayersList';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PlayersList from './PlayersList'
+import { render } from "@testing-library/react"
+
+
+
 
 it('PlayersList renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<PlayersList />, div);
   ReactDOM.unmountComponentAtNode(div);
+
 });
+
+it('PlayersList renders header to Dom', () => {
+  const { getByText } = render (<PlayersList />)
+  const header = getByText(/so many runners!/i)
+})
